d39651cac51a6f7f1e0fb29d697cb41f03c9edcb
wgw <1771893348@qq.com>
2018-12-26 23:03:12 +0800

    http网络架构

---
+++ httplibary/src/main/java/com/wcjr/buassistant/httplibary/HttpTaskRunnable.java
@@ -0,0 +1,8 @@
+package com.wcjr.buassistant.httplibary;
+
+public class HttpTaskRunnable implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
---
+++ httplibary/src/main/java/com/wcjr/buassistant/httplibary/ThreadManager.java
@@ -0,0 +1,51 @@
+package com.wcjr.buassistant.httplibary;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadManager  {
+    //把任务放到请求队列
+    private LinkedBlockingQueue<Runnable> mLinkedBlockingQueue = new LinkedBlockingQueue<>();
+    //add task
+    public void addTask(Runnable runnable){
+        if (null == runnable) return;
+        try {
+            mLinkedBlockingQueue.put(runnable);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    //把队列任务放入线程池
+    private ThreadPoolExecutor mThreadPoolExecutor;
+    private ThreadManager(){
+        mThreadPoolExecutor = new ThreadPoolExecutor(5,30,25,TimeUnit.SECONDS,new ArrayBlockingQueue<>(4),rejectedExecutionHandler);
+        mThreadPoolExecutor.execute(mRunnable);
+    }
+
+    private RejectedExecutionHandler rejectedExecutionHandler = new RejectedExecutionHandler() {
+        @Override
+        public void rejectedExecution(Runnable runnable, ThreadPoolExecutor threadPoolExecutor) {
+            try {
+                mLinkedBlockingQueue.put(runnable);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+    //开始工作
+    private Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            while (true){
+                try {
+                    mThreadPoolExecutor.execute(mLinkedBlockingQueue.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+}
